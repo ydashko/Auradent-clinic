75e6a7760289fb6cf8924c68b5155d2371b0499b
yaroslavdashko <yaroslav.dashkoa@gmail.com>
2026-03-21 13:12:32 +0200

    add menu animation

--- scripts/components/menu.js
+++ scripts/components/menu.js
@@ -1,19 +1,30 @@
 // export function initMenu() {
+
+// export function initMenu() {
 //   const dialog = document.querySelector(".tablet-overlay");
 //   const openBtn = document.querySelector(".burger-button");
 //   const closeBtn = document.querySelector(".cross-button");
 
 //   if (!dialog || !openBtn || !closeBtn) return;
 
-//   openBtn.addEventListener("click", () => {
+//   function openMenu() {
 //     dialog.showModal();
+//     dialog.classList.add("is-open");
 //     document.body.classList.add("no-scroll");
-//   });
+//   }
 
-//   closeBtn.addEventListener("click", () => {
+//   function closeMenu() {
 //     dialog.close();
 //     document.body.classList.remove("no-scroll");
-//   });
+
+//     dialog.addEventListener("transitionend", () => dialog.close(), {
+//       once: true,
+//     });
+//     document.body.classList.remove("no-scroll");
+//   }
+
+//   openBtn.addEventListener("click", openMenu);
+//   closeBtn.addEventListener("click", closeMenu);
 
 //   dialog.addEventListener("close", () => {
 //     document.body.classList.remove("no-scroll");
@@ -29,17 +40,26 @@ export function initMenu() {
 
   function openMenu() {
     dialog.showModal();
-    dialog.classList.add("is-open");
+
+    // даём браузеру применить display перед анимацией
+    requestAnimationFrame(() => {
+      dialog.classList.add("is-open");
+    });
+
     document.body.classList.add("no-scroll");
   }
 
   function closeMenu() {
-    dialog.close();
-    document.body.classList.remove("no-scroll");
+    dialog.classList.remove("is-open");
 
-    dialog.addEventListener("transitionend", () => dialog.close(), {
-      once: true,
-    });
+    dialog.addEventListener(
+      "transitionend",
+      () => {
+        dialog.close();
+      },
+      { once: true },
+    );
+
     document.body.classList.remove("no-scroll");
   }
 
